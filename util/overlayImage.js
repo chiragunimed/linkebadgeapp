50523577d7b4a1b6b19bac2515bef9ef12ef6029
chiragunimed <chiragunimed@gmail.com>
2025-07-30 23:46:19 -0400

    Add files via upload

---
+++ util/overlayImage.js
@@ -0,0 +1,27 @@
+const sharp = require("sharp");
+const path = require("path");
+const axios = require("axios");
+const fs = require("fs");
+
+async function overlayProfilePicture(profileUrl, email) {
+  const baseImagePath = path.join(__dirname, "../public/base_badge.png");
+  const outputImagePath = path.join(__dirname, `../public/output_${email}.png`);
+
+  const response = await axios.get(profileUrl, { responseType: "arraybuffer" });
+  const profileBuffer = Buffer.from(response.data);
+
+  const profileResized = await sharp(profileBuffer)
+    .resize(150, 150)
+    .circle()
+    .png()
+    .toBuffer();
+
+  await sharp(baseImagePath)
+    .composite([{ input: profileResized, top: 50, left: 50 }]) // adjust coordinates
+    .png()
+    .toFile(outputImagePath);
+
+  return outputImagePath;
+}
+
+module.exports = overlayProfilePicture;
